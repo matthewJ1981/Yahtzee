74fded95c0cdeedca1aa1fe8334bb190d248257e
Matthew <67207999+matthewJ1981@users.noreply.github.com>
2021-08-10 00:40:56 -0400

    Removed upper categories comments

--- Yahtzee/Yahtzee/UpperCategories.cpp
+++ Yahtzee/Yahtzee/UpperCategories.cpp
@@ -11,16 +11,6 @@ int Ones::CheckScore(const Dice& dice) const
 {
 	return UpperCheckScore(dice, 1);
 }
-//{
-//	if (HasScored())
-//		return UNSCORABLE;
-//
-//	int newScore = 0;
-//
-//	std::for_each(dice.begin(), dice.end(), [&](const Die& die) {if (die.Value() == 1) ++newScore; });
-//
-//	return newScore;
-//}
 
 Twos::Twos() :
 	Category("Twos")
@@ -30,16 +20,6 @@ int Twos::CheckScore(const Dice& dice) const
 {
 	return UpperCheckScore(dice, 2);
 }
-//{
-//	if (HasScored())
-//		return UNSCORABLE;
-//
-//	int newScore = 0;
-//
-//	std::for_each(dice.begin(), dice.end(), [&](const Die& die) {if (die.Value() == 2) newScore += 2; });
-//
-//	return newScore;
-//}
 
 Threes::Threes() :
 	Category("Threes")
@@ -49,16 +29,6 @@ int Threes::CheckScore(const Dice& dice) const
 {
 	return UpperCheckScore(dice, 3);
 }
-//{
-//	if (HasScored())
-//		return UNSCORABLE;
-//
-//	int newScore = 0;
-//
-//	std::for_each(dice.begin(), dice.end(), [&](const Die& die) {if (die.Value() == 3) newScore += 3; });
-//
-//	return newScore;
-//}
 
 Fours::Fours() :
 	Category("Fours")
@@ -68,16 +38,6 @@ int Fours::CheckScore(const Dice& dice) const
 {
 	return UpperCheckScore(dice, 4);
 }
-//{
-//	if (HasScored())
-//		return UNSCORABLE;
-//
-//	int newScore = 0;
-//
-//	std::for_each(dice.begin(), dice.end(), [&](const Die& die) {if (die.Value() == 4) newScore += 4; });
-//
-//	return newScore;
-//}
 
 Fives::Fives() :
 	Category("Fives")
@@ -87,16 +47,6 @@ int Fives::CheckScore(const Dice& dice) const
 {
 	return UpperCheckScore(dice, 5);
 }
-//{
-//	if (HasScored())
-//		return UNSCORABLE;
-//
-//	int newScore = 0;
-//
-//	std::for_each(dice.begin(), dice.end(), [&](const Die& die) {if (die.Value() == 5) newScore += 5; });
-//
-//	return newScore;
-//}
 
 Sixes::Sixes() :
 	Category("Sixes")
@@ -106,13 +56,3 @@ int Sixes::CheckScore(const Dice& dice) const
 {
 	return UpperCheckScore(dice, 6);
 }
-//{
-//	if (HasScored())
-//		return UNSCORABLE;
-//
-//	int newScore = 0;
-//
-//	std::for_each(dice.begin(), dice.end(), [&](const Die& die) {if (die.Value() == 6) newScore += 6; });
-//
-//	return newScore;
-//}
